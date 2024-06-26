114f4d9db53409016bb3bf330ec32b144fb668bf
professorjean <jean.isensee@edu.sc.senai.br>
2024-06-26 23:50:45 +0000

    Aula Actions 3

--- main_test.go
+++ main_test.go
@@ -27,7 +27,7 @@ func SetupDasRotasDeTeste() *gin.Engine {
 }
 
 func CriaAlunoMock() {
-	aluno := models.Aluno{Nome: "Nome do Aluno Teste", CPF: "12345678901", RG: "123456789"}
+	aluno := models.Aluno{Nome: "Nome do Aluno Genio", CPF: "12345678901", RG: "123456789"}
 	database.DB.Create(&aluno)
 	ID = int(aluno.ID)
 }
